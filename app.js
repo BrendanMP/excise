4dd12b54ecb66146e769ae3c3877a114af19a06a
Austin Miles <abm@austinbmiles.com>
2017-01-05 11:28:27 -0500

    added requires for passport, session, flash, mongoose

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
