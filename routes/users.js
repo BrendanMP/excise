5773abf40f4a240daaaaf154b04d1798240900ee
Austin Miles <abm@austinbmiles.com>
2017-01-09 20:11:53 -0500

    added middleware test for securing routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/userModel');
 
+router.use('/', function(req, res, next) {
+    if(!req.user){
+        res.redirect('/');
+    }
+    next();
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   User.find({}).sort('-createdAt')
